c790d82a5560fe70f7c82bff609d543d2209a5f4
Kaemyll <kaemyll.matsushima@gmail.com>
2021-12-04 14:44:18 +0100

    first commit

---
+++ randvlc.py
@@ -0,0 +1,18 @@
+# RANDVLC
+# Utilitaire de lancement de vidéos aléatoire depuis un répertoire et via VLC
+# Par Kaemyll - dec 2021
+
+import os, random, subprocess
+
+vidPath = str(input('Indiquez le chemin vers votre répertoire vidéo => '))
+
+def rndmp():
+   randomfile = random.choice(os.listdir(vidPath))
+   file = vidPath + randomfile
+   # todo - rendre modifiable le chemin de VLC, ainsi que la durée de start et run
+   subprocess.call(['C:\\Program Files (x86)\\VideoLAN\VLC\\vlc.exe ', file, '--start-time=30.0', ':run-time=60.0', '--play-and-exit', '--fullscreen'])
+   
+nbVideo = int(input('Combien de vidéos voulez-vous visionner ? '))
+
+for iter in range(0, nbVideo):
+   rndmp()
