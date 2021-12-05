8bef3593b9211bb723e9f38ef727fef3947d73c0
Kaemyll <kaemyll.matsushima@gmail.com>
2021-12-05 08:59:54 +0100

    dev: adding random start and run times

--- randvlc.py
+++ randvlc.py
@@ -5,14 +5,13 @@
 import os, random, subprocess
 
 vidPath = str(input('Indiquez le chemin vers votre répertoire vidéo => (Windows = \\) '))
+nbVideo = int(input('Combien de vidéos voulez-vous visionner ? '))
 
 def rndmp():
    randomfile = random.choice(os.listdir(vidPath))
    file = vidPath + randomfile
    # todo - rendre modifiable le chemin de VLC, ainsi que la durée de start et run
-   subprocess.call(['C:\\Program Files (x86)\\VideoLAN\VLC\\vlc.exe ', file, '--start-time=30.0', ':run-time=60.0', '--play-and-exit', '--fullscreen'])
-
-nbVideo = int(input('Combien de vidéos voulez-vous visionner ? '))
+   subprocess.call(['C:\\Program Files (x86)\\VideoLAN\VLC\\vlc.exe ', file, f'--start-time={random.randint(15,60)}', f':run-time={random.randint(30,120)}', '--play-and-exit', '--fullscreen'])
 
 for iter in range(0, nbVideo):
    rndmp()
